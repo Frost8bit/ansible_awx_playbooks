b9a5bb29e843b0a7bd6444b606cac693e0b93ce3
Frost8bit <frost.lost.ua@gmail.com>
2022-11-01 18:51:30 +0300

    Update inventory_source.sh

--- inventory_source.sh
+++ inventory_source.sh
@@ -1,27 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# id: scripts/trivial-inventory-script.sh
 
-ansible-inventory -i /dev/stdin --list <<PasteHERE
-[kube-master:children]
-master
-
-[kube-node:children]
-node
-
-[k8s-cluster:children]
-master
-node
-calico-rr
-
-[master]
-master1 ansible_host=192.168.90.212 etcd_member_name=etcd1
-
-[node]
-node1 ansible_host=192.168.90.214 
-node2 ansible_host=192.168.90.213 
-node3 ansible_host=192.168.90.211 
-
-[etcd:children]
-kube-master
-
-[calico-rr]
-PasteHERE
+cat << EOF
+{
+    "all": {
+        "hosts": ["web1.example.com", "web2.example.com"]
+    },
+    "_meta": {
+        "hostvars": {
+            "web1.example.com": {
+                "ansible_user": "rdiscala"
+            },
+            "web2.example.com": {
+                "ansible_user": "rdiscala"
+            }
+        }
+    }
+}
+EOF
